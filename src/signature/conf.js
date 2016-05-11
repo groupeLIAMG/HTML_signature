990296b80813ddf0c0c5af8cbb59db76a86f7214
Lorenzo Perozzi <lorenzo.perozzi@gmail.com>
2016-05-11 15:57:43 -0400

    logo fix
    absolute path to logo stored on github

--- src/signature/conf.js
+++ src/signature/conf.js
@@ -13,7 +13,7 @@ module.exports = [
     github: '<a href=""><img border="0" alt="linkedin" src="http://cdn.flaticon.com/png/256/25231.png" width="25" height="25"></a>', // Wrap phone numbers like this to prevent iOS mail from making them blue.
     twitter: '<a href=""><img border="0" alt="linkedin" src="https://g.twimg.com/about/feature-corporate/image/twitterbird_RGB.png" width="22" height="22"></a>', // Wrap phone numbers like this to prevent iOS mail from making them blue.
     contactMail: 'john.doe@gmail.com',
-    logoUrl: 'assets/liamg_logo.png',
+    logoUrl: 'https://raw.githubusercontent.com/groupeLIAMG/HTML_signature/master/src/signature/assets/liamg_logo.png',
     logoAlt: 'liamg logo',
     website: 'http://www.liamg.ca'
   }
